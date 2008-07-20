4e60472ed9824298256814c91f9f54d21899dee6
matt <unknown>
2008-07-20 14:02:48 +0000

    Checkstyle

--- src/main/java/uk/me/gumbley/minimiser/persistence/PersistenceObservableEvent.java
+++ src/main/java/uk/me/gumbley/minimiser/persistence/PersistenceObservableEvent.java
@@ -11,11 +11,18 @@ import uk.me.gumbley.commoncode.patterns.observer.ObservableEvent;
 public class PersistenceObservableEvent implements ObservableEvent {
     private String description;
 
+    /**
+     * Create the event with a description
+     * @param desc the description
+     */
     public PersistenceObservableEvent(final String desc) {
         description = desc;
     }
 
-    public String getDescription() {
+    /**
+     * @return the event description
+     */
+    public final String getDescription() {
         return description;
     }
 }
