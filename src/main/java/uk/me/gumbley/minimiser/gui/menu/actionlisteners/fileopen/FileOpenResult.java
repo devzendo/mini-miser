d5e8dd5f4355404530a3ce2c9e9cacceede492a7
matt <unknown>
2008-07-28 20:06:15 +0000

    Now uses the Opener to open, rather than embedding the opening functionality right here - increasing test coverage.

--- src/main/java/uk/me/gumbley/minimiser/gui/menu/actionlisteners/fileopen/FileOpenResult.java
+++ src/main/java/uk/me/gumbley/minimiser/gui/menu/actionlisteners/fileopen/FileOpenResult.java
@@ -14,9 +14,10 @@ import uk.me.gumbley.commoncode.string.StringUtils;
 import uk.me.gumbley.minimiser.gui.CursorManager;
 import uk.me.gumbley.minimiser.gui.dialog.PasswordEntryDialogHelper;
 import uk.me.gumbley.minimiser.gui.dialog.ProblemDialog;
+import uk.me.gumbley.minimiser.opener.Opener;
+import uk.me.gumbley.minimiser.opener.OpenerAdapter;
 import uk.me.gumbley.minimiser.openlist.OpenDatabaseList;
 import uk.me.gumbley.minimiser.persistence.AccessFactory;
-import uk.me.gumbley.minimiser.persistence.BadPasswordException;
 import uk.me.gumbley.minimiser.persistence.MiniMiserDatabase;
 import uk.me.gumbley.minimiser.persistence.MiniMiserDatabaseDescriptor;
 
@@ -45,86 +46,79 @@ public final class FileOpenResult extends DeferredWizardResult {
         this.cursorMan = cursorManager;
     }
     
+    private class FileOpenWizardOpenerAdapter implements OpenerAdapter {
+
+        private String dbName;
+        private final ResultProgressHandle progressHandle;
+
+        public FileOpenWizardOpenerAdapter(final ResultProgressHandle progress, final String name) {
+            this.progressHandle = progress;
+            dbName = name;
+        }
+
+        public void reportProgress(final ProgressStage progressStage, final String description) {
+            progressHandle.setProgress(description, progressStage.getValue(), progressStage.getMaximumValue());
+        }
+
+        public String requestPassword() {
+            // TODO pass the wizard's frame in here
+            final PasswordEntryDialogHelper passwordHelper = new PasswordEntryDialogHelper();
+            return passwordHelper.promptForPassword(null, dbName);
+        }
+        
+    }
     /**
      * {@inheritDoc}
      */
     @SuppressWarnings("unchecked")
     @Override
     public void start(final Map map, final ResultProgressHandle progress) {
-        progress.setProgress("Starting to open", 0, 3);
         final Map<String, Object> result = (Map<String, Object>) map;
         if (result == null) {
             LOGGER.info("User cancelled File|Open");
             return;
         }
-        LOGGER.debug("start with " + result);
         cursorMan.hourglassViaEventThread();
-
+        
         final String dbPath = (String) result.get(FileOpenWizardChooseFolderPage.PATH_NAME);
         // dbPath is the path to the directory - need to add the name of the directory
         // to the end, as that's the db name
         final File dbPathFile = new File(dbPath);
         final String dbName = dbPathFile.getName();
         final String dbFullPath = StringUtils.slashTerminate(dbPath) + dbName;
-        LOGGER.info("Opening database at path: " + dbFullPath);
-        boolean retryPasswordLoop = true;
-        // Try at first with an empty password - if we get a BPE, prompt for
-        // password and retry.
-        String dbPassword = "";
-        String tryingToOpenMessage = "Opening database";
-        while (retryPasswordLoop) {
-            try {
-                progress.setProgress(tryingToOpenMessage, 1, 3);
-                final MiniMiserDatabase database = access.openDatabase(dbFullPath, dbPassword);
-                LOGGER.info("Opened OK");
-                progress.setProgress("Opened OK", 3, 3);
-                
-                final Runnable addDatabaseSwingTask = new Runnable() {
-                    public void run() {
-                        LOGGER.info("Database created; adding to open database list");
-                        databaseList.addOpenedDatabase(new MiniMiserDatabaseDescriptor(dbName, dbFullPath, database));
-                    }
-                };
-                GUIUtils.runOnEventThread(addDatabaseSwingTask);
-                // A small delay to allow the user to notice the
-                // Opened OK progress result - otherwise the wizard just
-                // vanishes a little too abruptly.
-                ThreadUtils.waitNoInterruption(250);
-                retryPasswordLoop = false;
-            } catch (final BadPasswordException bad) {
-                LOGGER.warn("Bad password: " + bad.getMessage());
-                progress.setProgress("Password required", 2, 3);
-                // TODO pass the wizard's frame in here
-                final PasswordEntryDialogHelper passwordHelper = new PasswordEntryDialogHelper();
-                dbPassword = passwordHelper.promptForPassword(null, dbName);
-                if (dbPassword.equals("")) {
-                    LOGGER.info("Cancelled open (with password)");
-                    retryPasswordLoop = false;
+
+        final Opener opener = new Opener(access);
+        final OpenerAdapter openerAdapter = new FileOpenWizardOpenerAdapter(progress, dbName);
+        try {
+            final MiniMiserDatabase database = opener.openDatabase(dbName, dbFullPath, openerAdapter);
+            // TODO SMELL - this should just run, and the menu shoudl deal with dispatch on the EDT
+            databaseList.addOpenedDatabase(new MiniMiserDatabaseDescriptor(dbName, dbFullPath, database));
+//            final Runnable addDatabaseSwingTask = new Runnable() {
+//                public void run() {
+//                    LOGGER.info("Database created; adding to open database list");
+//                    databaseList.addOpenedDatabase(new MiniMiserDatabaseDescriptor(dbName, dbFullPath, database));
+//                }
+//            };
+//            GUIUtils.runOnEventThread(addDatabaseSwingTask);
+            // A small delay to allow the user to notice the
+            // Opened OK progress result - otherwise the wizard just
+            // vanishes a little too abruptly.
+            ThreadUtils.waitNoInterruption(250);
+        } catch (final DataAccessResourceFailureException darfe) {
+            LOGGER.warn("Could not open database: " + darfe.getMessage());
+            // TODO pass the wizard's frame in here
+            GUIUtils.invokeLaterOnEventThread(new Runnable() {
+                public void run() {
+                    JOptionPane.showMessageDialog(null,
+                        "Could not open database '" + dbName + "':\n" + darfe.getMessage(),
+                        "Could not open database '" + dbName + "'",
+                        JOptionPane.OK_OPTION);
                 }
-                tryingToOpenMessage = "Trying to open database";
-            } catch (final DataAccessResourceFailureException darfe) {
-                LOGGER.warn("Could not open database: " + darfe.getMessage());
-                // TODO pass the wizard's frame in here
-                GUIUtils.invokeLaterOnEventThread(new Runnable() {
-                    public void run() {
-                        JOptionPane.showMessageDialog(null,
-                            "Could not open database '" + dbName + "':\n" + darfe.getMessage(),
-                            "Could not open database '" + dbName + "'",
-                            JOptionPane.OK_OPTION);
-                    }
-                });
-                retryPasswordLoop = false;
-                // don't retry file choice - shouldn't have been able to open
-                // rubbish anyway
-            } catch (final DataAccessException dae) {
-                // TODO report error to user
-                // WOZERE the whole app goes ape if an exception is thrown, and clobbers eclipse
-                
-                LOGGER.warn("Data access exception: " + dae.getMessage(), dae);
-                // TODO pass the wizard's frame in here
-                ProblemDialog.reportProblem(null, "trying to open database '" + dbName + "'.", dae);
-                retryPasswordLoop = false;
-            }
+            });
+        } catch (final DataAccessException dae) {
+            LOGGER.warn("Data access exception: " + dae.getMessage(), dae);
+            // TODO pass the wizard's frame in here
+            ProblemDialog.reportProblem(null, "trying to open database '" + dbName + "'.", dae);
         }
         progress.finished(null);
         cursorMan.normalViaEventThread();
