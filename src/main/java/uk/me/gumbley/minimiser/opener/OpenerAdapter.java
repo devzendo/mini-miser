aa38649d9b2017a0d7ddaab78134fcf6d188b4d7
matt <unknown>
2008-07-28 20:05:20 +0000

    Changed the ProgressState enum to a class, so it can have methods. Could probably have kept an enum, but this is a little more flexible.

--- src/main/java/uk/me/gumbley/minimiser/opener/OpenerAdapter.java
+++ src/main/java/uk/me/gumbley/minimiser/opener/OpenerAdapter.java
@@ -11,42 +11,65 @@ package uk.me.gumbley.minimiser.opener;
 public interface OpenerAdapter {
 
     /**
-     * Sent by the progress notifications.
+     * Sent by the progress notifications, this denotes a stage in the open
+     * operation.
      * @author matt
      *
      */
-    enum ProgressStage {
+    public final class ProgressStage {
+        private int enumValue;
+        private ProgressStage(final int val) {
+            enumValue = val;
+        }
+        
+        /**
+         * What is the value of this stage in the overall progress?
+         * Progress is measured as starting from 0, and ending at getMaximumValue().
+         * @return the value of the stage
+         */
+        public int getValue() {
+            return enumValue;
+        }
+
+        /**
+         * How many steps are there in total?
+         * @return the maximum value of a ProgressStage 
+         */
+        public int getMaximumValue() {
+            return 3;
+        }
+        
         /**
          * The open operation is starting. Sent almost immediately to give some
          * immediate feedback.
          */
-        STARTING, // start 0
+        public static final ProgressStage STARTING = new ProgressStage(0);
         /**
          * Sent immediately prior to opening the database. 
          */
-        OPENING, // 1
+        public static final ProgressStage OPENING = new ProgressStage(1);
 
         /**
          * Sent before the password is requested from the adapter. 
          */
-        PASSWORD_REQUIRED, // 2 
+        public static final ProgressStage PASSWORD_REQUIRED = new ProgressStage(2); 
 
         /**
          * Sent upon successful open. 
          */
-        OPENED, // 3
+        public static final ProgressStage OPENED = new ProgressStage(3);
         /**
          * The user cancelled the password entry on an encrypted database. 
          */
-        PASSWORD_CANCELLED, // 3
+        public static final ProgressStage PASSWORD_CANCELLED = new ProgressStage(3);
         /**
          * The database is not present. 
          */
-        NOT_PRESENT, // 3
+        public static final ProgressStage NOT_PRESENT = new ProgressStage(3);
         /**
          * Failed to open for a serious reason 
          */
-        OPEN_FAILED // 3
+        public static final ProgressStage OPEN_FAILED = new ProgressStage(3);
     };
     
     /**
