6af1c5bf1e5828ce93b31b16b0c986a28e5f0e2f
matt <unknown>
2008-11-06 23:04:34 +0000

    Missed this Welcome Dialog helper from last commit.

---
+++ src/main/java/uk/me/gumbley/minimiser/gui/dialog/welcome/WelcomeDialogHelper.java
@@ -0,0 +1,56 @@
+package uk.me.gumbley.minimiser.gui.dialog.welcome;
+
+import java.awt.Frame;
+import javax.swing.SwingUtilities;
+import uk.me.gumbley.minimiser.gui.CursorManager;
+import uk.me.gumbley.minimiser.springloader.SpringLoader;
+
+/**
+ * Toolkit for easily creating Welcome Dialogs.
+ * 
+ * @author matt
+ *
+ */
+public final class WelcomeDialogHelper {
+    private static Frame parentFrame;
+    private static CursorManager cursorManager;
+
+    /**
+     * 
+     */
+    private WelcomeDialogHelper() {
+        // no instances
+    }
+    
+    /**
+     * Initialise the toolkit by obtaining the things it needs to create
+     * Welcome dialogs.
+     * 
+     * @param springLoader the spring loader
+     */
+    public static void initialise(final SpringLoader springLoader) {
+        parentFrame = springLoader.getBean("mainFrame", Frame.class);
+        cursorManager = springLoader.getBean("cursorManager", CursorManager.class);
+    }
+    
+    /**
+     * Show the welcome dialog
+     */
+    public static void showWelcomeDialog() {
+        showDialog(true);
+    }
+    
+    /**
+     * Show the what's new dialog 
+     */
+    public static void showWhatsNewDialog() {
+        showDialog(false);
+    }
+    
+    private static void showDialog(final boolean isWelcome) {
+        assert SwingUtilities.isEventDispatchThread();
+        final WelcomeDialog welcome = new WelcomeDialog(parentFrame, cursorManager, isWelcome);
+        welcome.pack();
+        welcome.setVisible(true);
+    }
+}
