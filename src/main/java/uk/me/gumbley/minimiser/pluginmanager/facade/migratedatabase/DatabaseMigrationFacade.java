3322fb0fe754396fc87531debbce1b4af8607378
matt <unknown>
2009-08-10 20:45:41 +0000

    Started defining the database migration facade.

---
+++ src/main/java/uk/me/gumbley/minimiser/pluginmanager/facade/migratedatabase/DatabaseMigrationFacade.java
@@ -0,0 +1,11 @@
+package uk.me.gumbley.minimiser.pluginmanager.facade.migratedatabase;
+
+/**
+ * Facade that allows plugins to upgrade their databases to the
+ * latest schema upon opening.
+ * 
+ * @author matt
+ *
+ */
+public interface DatabaseMigrationFacade {
+}
