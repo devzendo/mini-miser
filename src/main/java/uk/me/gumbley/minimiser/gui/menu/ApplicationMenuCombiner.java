2de71006fdb205fc7a9402cf6a44106aa732e2ce
matt <unknown>
2010-01-18 21:06:40 +0000

    Added the ApplicationMenuCombiner.

---
+++ src/main/java/uk/me/gumbley/minimiser/gui/menu/ApplicationMenuCombiner.java
@@ -0,0 +1,88 @@
+package uk.me.gumbley.minimiser.gui.menu;
+
+import java.util.List;
+
+import javax.swing.JComponent;
+import javax.swing.JMenu;
+
+import uk.me.gumbley.minimiser.gui.menu.ApplicationMenu.SystemMenu;
+import uk.me.gumbley.minimiser.openlist.DatabaseDescriptor;
+import uk.me.gumbley.minimiser.openlist.OpenDatabaseList;
+import uk.me.gumbley.minimiser.openlist.DatabaseDescriptor.AttributeIdentifier;
+
+/**
+ * Combines the global ApplicationMenu and the current database's ApplicationMenu.
+ *
+ * @author matt
+ *
+ */
+public final class ApplicationMenuCombiner {
+    private static final ApplicationMenu EMPTY_APPLICATION_MENU = new ApplicationMenu();
+    private final ApplicationMenu mGlobalApplicationMenu;
+    private final OpenDatabaseList mOpenDatabaseList;
+
+    /**
+     * @param globalApplicationMenu the Global ApplicationMenu used in the combining of
+     * all database ApplicationMenus
+     * @param openDatabaseList the OpenDatabaseList from where the current database's
+     * ApplicationMenu will be retrieved
+     */
+    public ApplicationMenuCombiner(
+            final ApplicationMenu globalApplicationMenu,
+            final OpenDatabaseList openDatabaseList) {
+        mGlobalApplicationMenu = globalApplicationMenu;
+        mOpenDatabaseList = openDatabaseList;
+    }
+
+    /**
+     * Combine the global ApplicationMenu and the current database's ApplicationMenu
+     * (if there is a current database, and if it has provided an ApplicationMenu) into
+     * a single ApplicationMenu, with the items from the global menu taking priority
+     * over those from the database menu.
+     * @return the combined ApplicationMenu
+     */
+    public ApplicationMenu combineMenus() {
+        final ApplicationMenu combinedMenu = new ApplicationMenu();
+        combineCustomMenus(combinedMenu);
+        for (final ApplicationMenu.SystemMenu systemMenu : ApplicationMenu.SystemMenu.values()) {
+            combineSystemMenus(combinedMenu, systemMenu);
+        }
+        return combinedMenu;
+    }
+
+    private void combineSystemMenus(
+            final ApplicationMenu combinedMenu,
+            final SystemMenu systemMenu) {
+        addSystemMenuToCombinedMenu(combinedMenu, systemMenu, mGlobalApplicationMenu.getMenu(systemMenu));
+        addSystemMenuToCombinedMenu(combinedMenu, systemMenu, getDatabaseApplicationMenu().getMenu(systemMenu));
+    }
+
+    private void addSystemMenuToCombinedMenu(
+            final ApplicationMenu combinedMenu,
+            final SystemMenu systemMenu,
+            final List<JComponent> systemMenuComponents) {
+        for (final JComponent systemMenuComponent : systemMenuComponents) {
+            combinedMenu.addMenuComponent(systemMenu, systemMenuComponent);
+        }
+    }
+
+    private void combineCustomMenus(final ApplicationMenu combinedMenu) {
+        for (final JMenu globalJMenu : mGlobalApplicationMenu.getCustomMenus()) {
+            combinedMenu.addCustomMenu(globalJMenu);
+        }
+        for (final JMenu databaseJMenu : getDatabaseApplicationMenu().getCustomMenus()) {
+            combinedMenu.addCustomMenu(databaseJMenu);
+        }
+    }
+
+    private ApplicationMenu getDatabaseApplicationMenu() {
+        final DatabaseDescriptor descriptor = mOpenDatabaseList.getCurrentDatabase();
+        if (descriptor != null) {
+            final ApplicationMenu databaseApplicationMenu = (ApplicationMenu) descriptor.getAttribute(AttributeIdentifier.ApplicationMenu);
+            if (databaseApplicationMenu != null) {
+                return databaseApplicationMenu;
+            }
+        }
+        return EMPTY_APPLICATION_MENU;
+    }
+}
--- src/test/java/uk/me/gumbley/minimiser/gui/menu/TestApplicationMenuCombiner.java
+++ src/test/java/uk/me/gumbley/minimiser/gui/menu/TestApplicationMenuCombiner.java
@@ -1,5 +1,20 @@
 package uk.me.gumbley.minimiser.gui.menu;
 
+import java.util.List;
+
+import javax.swing.JComponent;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JSeparator;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.me.gumbley.minimiser.openlist.DatabaseDescriptor;
+import uk.me.gumbley.minimiser.openlist.OpenDatabaseList;
+import uk.me.gumbley.minimiser.openlist.DatabaseDescriptor.AttributeIdentifier;
+
 
 /**
  * Tests for the combining of global application menu and database-specific application menu.
@@ -8,4 +23,115 @@ package uk.me.gumbley.minimiser.gui.menu;
  *
  */
 public final class TestApplicationMenuCombiner {
+    private ApplicationMenu mGlobalApplicationMenu;
+    private OpenDatabaseList mOpenDatabaseList;
+    private ApplicationMenuCombiner mApplicationMenuCombiner;
+
+    /**
+     *
+     */
+    @Before
+    public void getPrerequisites() {
+        mGlobalApplicationMenu = new ApplicationMenu();
+        mOpenDatabaseList = new OpenDatabaseList();
+        mApplicationMenuCombiner = new ApplicationMenuCombiner(mGlobalApplicationMenu, mOpenDatabaseList);
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void emptyApplicationMenus() {
+        Assert.assertNull(mOpenDatabaseList.getCurrentDatabase());
+
+        final ApplicationMenu menu = mApplicationMenuCombiner.combineMenus();
+
+        Assert.assertEquals(0, menu.getCustomMenus().size());
+        for (final ApplicationMenu.SystemMenu systemMenu : ApplicationMenu.SystemMenu.values()) {
+            final List<JComponent> systemMenuList = menu.getMenu(systemMenu);
+            Assert.assertEquals(0, systemMenuList.size());
+        }
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void customMenusFromGlobalAreOrderedFirst() {
+        mGlobalApplicationMenu.addCustomMenu(new JMenu("GlobalCustom"));
+        final ApplicationMenu databaseApplicationMenu = new ApplicationMenu();
+        databaseApplicationMenu.addCustomMenu(new JMenu("DatabaseCustom"));
+        setCurrentDatabaseApplicationMenu(databaseApplicationMenu);
+        Assert.assertNotNull(mOpenDatabaseList.getCurrentDatabase());
+
+        final ApplicationMenu menu = mApplicationMenuCombiner.combineMenus();
+
+        final List<JMenu> customMenus = menu.getCustomMenus();
+        Assert.assertEquals(2, customMenus.size());
+        Assert.assertEquals("GlobalCustom", customMenus.get(0).getText());
+        Assert.assertEquals("DatabaseCustom", customMenus.get(1).getText());
+    }
+
+    private void setCurrentDatabaseApplicationMenu(
+            final ApplicationMenu applicationMenu) {
+        final DatabaseDescriptor descriptor = new DatabaseDescriptor("FOO");
+        descriptor.setAttribute(AttributeIdentifier.ApplicationMenu, applicationMenu);
+        mOpenDatabaseList.addOpenedDatabase(descriptor);
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void separatorsBetweenGlobalAndDatabaseAreCoalesced() {
+        mGlobalApplicationMenu.addMenuComponent(ApplicationMenu.SystemMenu.File, new JSeparator());
+        final ApplicationMenu databaseApplicationMenu = new ApplicationMenu();
+        databaseApplicationMenu.addMenuComponent(ApplicationMenu.SystemMenu.File, new JSeparator());
+        setCurrentDatabaseApplicationMenu(databaseApplicationMenu);
+
+        final ApplicationMenu menu = mApplicationMenuCombiner.combineMenus();
+
+        final List<JComponent> fileMenu = menu.getMenu(ApplicationMenu.SystemMenu.File);
+        Assert.assertEquals(1, fileMenu.size());
+        Assert.assertTrue(fileMenu.get(0) instanceof JSeparator);
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void menuItemsFromGlobalAreOrderedFirst() {
+        mGlobalApplicationMenu.addMenuComponent(ApplicationMenu.SystemMenu.File, new JMenuItem("GlobalMenuItem"));
+        final ApplicationMenu databaseApplicationMenu = new ApplicationMenu();
+        databaseApplicationMenu.addMenuComponent(ApplicationMenu.SystemMenu.File, new JMenuItem("DatabaseMenuItem"));
+        setCurrentDatabaseApplicationMenu(databaseApplicationMenu);
+
+        final ApplicationMenu menu = mApplicationMenuCombiner.combineMenus();
+
+        final List<JComponent> fileMenu = menu.getMenu(ApplicationMenu.SystemMenu.File);
+        Assert.assertEquals(2, fileMenu.size());
+        Assert.assertEquals("GlobalMenuItem", ((JMenuItem) fileMenu.get(0)).getText());
+        Assert.assertEquals("DatabaseMenuItem", ((JMenuItem) fileMenu.get(1)).getText());
+
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void closingDatabaseJustYieldsGlobal() {
+        mGlobalApplicationMenu.addMenuComponent(ApplicationMenu.SystemMenu.File, new JMenuItem("GlobalMenuItem"));
+        final ApplicationMenu databaseApplicationMenu = new ApplicationMenu();
+        databaseApplicationMenu.addMenuComponent(ApplicationMenu.SystemMenu.File, new JMenuItem("DatabaseMenuItem"));
+        setCurrentDatabaseApplicationMenu(databaseApplicationMenu);
+        final DatabaseDescriptor currentDatabase = mOpenDatabaseList.getCurrentDatabase();
+        Assert.assertNotNull(currentDatabase);
+        mOpenDatabaseList.removeClosedDatabase(currentDatabase);
+
+        final ApplicationMenu menu = mApplicationMenuCombiner.combineMenus();
+
+        final List<JComponent> fileMenu = menu.getMenu(ApplicationMenu.SystemMenu.File);
+        Assert.assertEquals(1, fileMenu.size());
+        Assert.assertEquals("GlobalMenuItem", ((JMenuItem) fileMenu.get(0)).getText());
+    }
 }
