8fa178221850fcde1d05c407dbbce9876d7d52bf
matt <unknown>
2010-01-31 19:21:09 +0000

    Initial thoughts on the View Menu

---
+++ docs/mindmaps/View Menu.mm
@@ -0,0 +1,31 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1264843995720" ID="Freemind_Link_986927298" MODIFIED="1264844000050" TEXT="View Menu">
+<node CREATED="1264844002303" ID="_" MODIFIED="1264844007636" POSITION="right" TEXT="TabIdentifiers">
+<node CREATED="1264844008528" ID="Freemind_Link_941233066" MODIFIED="1264844015269" TEXT="Permanent / Transient"/>
+<node CREATED="1264844015512" ID="Freemind_Link_1276409710" MODIFIED="1264844020764" TEXT="System / Plugin">
+<node CREATED="1264844035601" ID="Freemind_Link_293477662" MODIFIED="1264844037804" TEXT="System">
+<node CREATED="1264844038569" ID="Freemind_Link_1045613540" MODIFIED="1264844045556" TEXT="Set declared in SystemTabIdentifiers"/>
+<node CREATED="1264844086450" ID="Freemind_Link_1767964490" MODIFIED="1264844091693" TEXT="And built into the menu"/>
+</node>
+<node CREATED="1264844050641" ID="Freemind_Link_5075250" MODIFIED="1264844052556" TEXT="Plugin">
+<node CREATED="1264844052897" ID="Freemind_Link_1498797893" MODIFIED="1264844059157" TEXT="Sets added to ApplicationMenu"/>
+<node CREATED="1264844059561" ID="Freemind_Link_1311432261" MODIFIED="1264844063758" TEXT="In the View Menu"/>
+<node CREATED="1264844064201" ID="Freemind_Link_595310629" MODIFIED="1264844073765" TEXT="Are combined by the ApplicationMenuCombiner"/>
+<node CREATED="1264844074233" ID="Freemind_Link_518388177" MODIFIED="1264844085109" TEXT="And built into the menu"/>
+</node>
+</node>
+<node CREATED="1264844021145" ID="Freemind_Link_44842435" MODIFIED="1264844028028" TEXT="Static / Dynamic">
+<node CREATED="1264844095634" ID="Freemind_Link_917624587" MODIFIED="1264844101782" TEXT="Static">
+<node CREATED="1264844102770" ID="Freemind_Link_128953106" MODIFIED="1264844156953" TEXT="Have Spring beans with&#xa;prescribed, parameterisable&#xa;names"/>
+<node CREATED="1264844121259" ID="Freemind_Link_903677503" MODIFIED="1264844129702" TEXT="Loaded by a TabLoader (?)"/>
+<node CREATED="1264844130099" ID="Freemind_Link_1614185914" MODIFIED="1264844144978" TEXT="Currently the only mechanism&#xa;for loading tabs"/>
+</node>
+<node CREATED="1264844160413" ID="Freemind_Link_1523895271" MODIFIED="1264844162607" TEXT="Dynamic">
+<node CREATED="1264844163419" ID="Freemind_Link_241914261" MODIFIED="1264844179810" TEXT="Loading is delegated to an application&#xa;component"/>
+<node CREATED="1264844181172" ID="Freemind_Link_1258294205" MODIFIED="1264844191703" TEXT="Actual loading can be aided by the framework"/>
+</node>
+</node>
+</node>
+</node>
+</map>
